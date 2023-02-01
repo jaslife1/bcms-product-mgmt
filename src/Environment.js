4d4f1af74f0c542d65b3fb3ed05fbb757bd4d165
Jason Buhay <buhayjason@gmail.com>
2023-02-01 20:05:52 +0800

    Create Relay Environment configuration

---
+++ src/Environment.js
@@ -0,0 +1,37 @@
+// Import modules needed to instantiate and configure Relay environment
+const {
+    Environment,
+    Network,
+    RecordSource,
+    Store,
+  } = require('relay-runtime')
+  
+  // Instantiate Store for cache data
+  const store = new Store(new RecordSource())
+  
+  // Create network that knows the GraphQL server. Returns a promise
+  const network = Network.create((operation, variables) => {
+    // Server endpoint
+    return fetch('__RELAY_API_ENDPOINT__', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: operation.text,
+        variables,
+      }),
+    }).then(response => {
+      return response.json()
+    })
+  })
+  
+  // Instantiate actual Environment
+  const environment = new Environment({
+    network,
+    store,
+  })
+  
+  // export module
+  export default environment
