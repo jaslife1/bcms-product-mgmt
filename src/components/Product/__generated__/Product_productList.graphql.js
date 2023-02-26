342207929204c974a7f9de574f0e7fb571067451
Jason Buhay <buhayjason@gmail.com>
2023-02-26 14:11:28 +0800

    Added the missing generated productList graphql

---
+++ src/components/Product/__generated__/Product_productList.graphql.js
@@ -0,0 +1,88 @@
+/**
+ * @generated SignedSource<<4213495aacb57757a88a4636b384ae1d>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+var node = {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "Product_productList",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "Product",
+      "kind": "LinkedField",
+      "name": "product",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "id",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "name",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "sku",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "price",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "barcode",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "ProductInventory",
+      "kind": "LinkedField",
+      "name": "inventory",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "quantity",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "type": "ProductList",
+  "abstractKey": null
+};
+
+node.hash = "11bdd8ed652332c2fd41add998ef92c4";
+
+module.exports = node;
